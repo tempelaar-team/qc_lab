11463c88ba4717e76f7e6ea3f5b2859f00b81098
akrotz <alexkrotz@gmail.com>
2025-03-20 11:40:50 -0500

    updated formatting.

--- examples/mpi_examples/mpi_example.py
+++ examples/mpi_examples/mpi_example.py
@@ -13,9 +13,9 @@ In this example num_tasks is determined automatically by the mpi driver.
 import numpy as np
 import matplotlib.pyplot as plt
 from mpi4py import MPI
-from qc_lab import Simulation 
+from qc_lab import Simulation
 from qc_lab.models import SpinBoson
-from qc_lab.algorithms import MeanField 
+from qc_lab.algorithms import MeanField
 from qc_lab.dynamics import parallel_driver_mpi
 
 # instantiate a simulation
@@ -27,11 +27,11 @@ sim.settings.batch_size = 100
 sim.settings.tmax = 10
 sim.settings.dt = 0.001
 
-# instantiate a model 
+# instantiate a model
 sim.model = SpinBoson()
-# instantiate an algorithm 
+# instantiate an algorithm
 sim.algorithm = MeanField()
-# define an initial diabatic wavefunction 
+# define an initial diabatic wavefunction
 sim.state.wf_db = np.array([1, 0], dtype=complex)
 
 data = parallel_driver_mpi(sim)
@@ -40,4 +40,4 @@ data = parallel_driver_mpi(sim)
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 if rank == 0:
-    print(data.data_dic['seed'])
+    print(data.data_dic["seed"])
