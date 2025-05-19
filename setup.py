d018257cdca362789038d7498bd26ef8dde21788
akrotz <alexkrotz@gmail.com>
2025-05-19 18:28:29 -0500

    Added tests. Updated setup.py and CI workflow.

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup.py
 
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0a3'
-DESCRIPTION = 'QC Lab: a python package for quantum-classical modeling.'
-LONG_DESCRIPTION = 'QC Lab is a python package for quantum-classical modeling. It was developed in 2025 by the Tempelaar Team at Northwestern University.'
-
+VERSION = "0.1.0a3"
+DESCRIPTION = "QC Lab: a python package for quantum-classical modeling."
+LONG_DESCRIPTION = "QC Lab is a python package for quantum-classical modeling. It was developed in 2025 by the Tempelaar Team at Northwestern University."
 setup(
     name="qc_lab",
     version=VERSION,
@@ -16,9 +15,11 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['numpy<2.1,>=1.22', 'tqdm','h5py','numba','matplotlib'],
-    keywords=['surface hopping', 'mixed quantum-classical dynamics',
-              'theoretical chemistry', 'ehrenfest', 'python', 'quantum-classical'],
+    python_requires=">3.8,<3.13",
+    install_requires=["numpy<=1.26,>=1.22", "tqdm","h5py","numba<=0.60,>=0.58","matplotlib"],
+    extras_requires={"test":["pytest"]},
+    keywords=["surface hopping", "mixed quantum-classical dynamics",
+              "theoretical chemistry", "ehrenfest", "python", "quantum-classical"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
---
+++ tests/test_drivers.py
@@ -0,0 +1,40 @@
+""" 
+This module tests the serial and multiprocessing drivers for a few simple cases.
+"""
+import pytest
+
+def test_drivers_spinboson():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from qc_lab import Simulation # import simulation class 
+    from qc_lab.models import SpinBoson # import model class 
+    from qc_lab.algorithms import MeanField # import algorithm class 
+    from qc_lab.dynamics import serial_driver, parallel_driver_multiprocessing # import dynamics driver
+
+    sim = Simulation()
+    sim.settings.num_trajs = 200
+    sim.settings.batch_size = 50
+    sim.settings.tmax = 10
+    sim.settings.dt = 0.01
+
+    sim.model = SpinBoson({
+        'V':0.5,
+        'E':0.5,
+        'A':100,
+        'W':0.1,
+        'l_reorg':0.005,
+        'boson_mass':1.0,
+        'kBT':1.0,
+
+    })
+    sim.algorithm = MeanField()
+
+    sim.state.wf_db= np.zeros((sim.model.constants.num_quantum_states), dtype=complex)
+    sim.state.wf_db[0] += 1.0
+
+    data_serial = serial_driver(sim)
+    data_parallel = parallel_driver_multiprocessing(sim)
+    for key, val in data_serial.data_dict.items():
+        if isinstance(val, np.ndarray):
+            assert np.allclose(val, data_parallel.data_dict[key])
+    return
